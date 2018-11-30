f8b5bc09d8485077c58f20f85ff7962ab34b4a0f
Vaughn Ramsey <vramsey20@slp-vramsey20.local>
2018-11-30 15:04:07 -0700

    added compression rate

--- huffman.py
+++ huffman.py
@@ -1,10 +1,12 @@
 from Queue import PriorityQueue
+import os
 class Node:
 	def __init__(self, key, value, upPointer, downPointer):
 		self.upPointer = upPointer
 		self.downPointer = downPointer
 		self.key = key
 		self.value = value
+		self.depth = 0
 
 	def __repr__(self):
 		return "Node{"+"freq="+str(self.getKey())+", char="+str(self.getValue())+"}"
@@ -23,15 +25,20 @@ class Node:
 	def isLeaf(self):
 		return self.downPointer == None and self.upPointer == None
 	def __cmp__(self, other):
-		if(self.getKey()<other.getKey()):
-			return -1
-		elif(self.getKey()==other.getKey()):
-			return 0
+		if(self is not None and other is not None):
+			if(self.getKey()<other.getKey()):
+				return -1
+			elif(self.getKey()==other.getKey()):
+				return 0
+			return 1
 		return 1
+
+
 class Huffman():
 	def __init__(self, string):
 		self.root = None
 		self.queue = PriorityQueue()
+		self.myArr = []
 		arr = {}
 		for char in string:
 			if(char in arr):
@@ -41,31 +48,59 @@ class Huffman():
 		for key in arr:
 			self.queue.put(Node(arr[key], key, None, None))
 		self.construct()
-	def __repr__(self):
-		"[" + self.toString(self.root) + "]"
-	def toString(self, root):
-		print(self.root)
+	def setDepth(self):
+		self.setDepthRec(self.root, 1)
+	def setDepthRec(self, root, size):
 		if(root.isLeaf()):
-			return str(root)
+			root.depth = size
+			return 1
 		else:
+			size += 1
 			if(root.getUp()!=None and root.getDown!=None):
-				return str(root) + self.toString(root.getUp()) + self.toString(root.getDown())
+				return self.setDepthRec(root.getUp(), size) + self.setDepthRec(root.getDown(), size)
 			elif(root.getUp!=None):
-				return str(root) + self.toString(root.getUp())
+				return self.setDepthRec(root.getUp(), size)
 			else:
-				return str(root) + self.toString(root.getDown())
+				return self.setDepthRec(root.getDown(), size)
+
+	def __str__(self):
+		#return str("[" + self.toString(self.root) + "]")
+		return str(self.myArr[::-1])
+	
 
 	def construct(self):
 		while(self.queue.qsize()!=1):			
 			first = self.queue.get()
-			print(first)
-
 			if(self.queue.qsize()!=0):
 				second = self.queue.get()
 			else:
 				second = Node(0, "-1", None, None)
 			newElem = Node(first.getKey()+second.getKey(), "*", first, second)
+			self.myArr.append(first)
+			self.myArr.append(second)
 			self.queue.put(newElem)
 		self.root = self.queue.get()
-huffman = Huffman("hello there I am vaughns")
-print(huffman)
+		self.myArr.append(self.root)
+		self.setDepth()
+
+
+
+filename = "txt.txt"
+file = open(filename, "r")
+lines = file.read()
+lines.replace("\n", "\\n")
+huffman = Huffman(lines)
+print(str(huffman))
+i = 0
+for char in lines :
+	i += 1
+i *= 32
+j = 0
+for item in huffman.myArr:
+	if(item.isLeaf()):
+		j+=item.key*item.depth
+print(1.0-(1.0*j/i))
+
+
+
+
